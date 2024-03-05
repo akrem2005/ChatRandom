edfffb8493c8d715fd0172a3746346876854338e
akrem2005 <lighttech96@gmail.com>
2024-03-05 11:35:04 +0000

    f

--- app.js
+++ app.js
@@ -6,34 +6,24 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.use(express.static("public"));
+app.use(express.static("public")); // Assuming your HTML file is in a 'public' folder
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-// Socket.io logic
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("A user connected");
 
-  // Handle joining a room
   socket.on("join-room", (room) => {
     socket.join(room);
-    console.log(`User joined room ${room}`);
   });
 
-  // Handle signaling data
   socket.on("signal", (data) => {
     io.to(data.room).emit("signal", data);
   });
 
-  // Handle disconnect
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    console.log("A user disconnected");
   });
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
