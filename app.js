fe274409c184af31adf7e983ff687e0aae6dc06b
akrem2005 <lighttech96@gmail.com>
2024-03-05 11:14:06 +0000

    f

--- app.js
+++ app.js
@@ -6,62 +6,34 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// Keep track of users in rooms
-const usersInRooms = {};
-
+// Socket.io logic
 io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
-
-  // Handle disconnection
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-    // Remove the user from the usersInRooms object
-    if (usersInRooms[socket.id]) {
-      const room = usersInRooms[socket.id].room;
-      delete usersInRooms[socket.id];
-      // Notify other users in the room about the disconnection
-      io.to(room).emit("userDisconnected", { disconnectedID: socket.id, room });
-    }
-  });
-
-  // Handle call initiation
-  socket.on("call", ({ callerID, calleeID, room }) => {
-    // Notify the callee about the incoming call
-    io.to(room).emit("incomingCall", { callerID, room });
-  });
-
-  // Handle answering the call
-  socket.on("answerCall", ({ callerID, answererID, room }) => {
-    // Notify the caller that the call is answered
-    io.to(room).emit("callAnswered", { answererID, room });
-  });
-
-  // Handle sending and receiving video stream
-  socket.on("offer", (data) => {
-    io.to(data.room).emit("offer", data);
-  });
-
-  socket.on("answer", (data) => {
-    io.to(data.room).emit("answer", data);
-  });
-
-  socket.on("ice-candidate", (data) => {
-    io.to(data.room).emit("ice-candidate", data.candidate);
-  });
+  console.log("a user connected");
 
   // Handle joining a room
-  socket.on("joinRoom", (room) => {
+  socket.on("join-room", (room) => {
     socket.join(room);
-    usersInRooms[socket.id] = { room };
-    // Notify other users in the room about the new connection
-    io.to(room).emit("userConnected", { connectedID: socket.id, room });
+    console.log(`User joined room ${room}`);
+  });
+
+  // Handle signaling data
+  socket.on("signal", (data) => {
+    io.to(data.room).emit("signal", data);
+  });
+
+  // Handle disconnect
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
+// Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
